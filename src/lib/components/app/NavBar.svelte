62b892b86d6364441ec5b411c7af1b144498f8c7
Leo <leowasntfound@gmail.com>
2025-01-14 14:37:28 +0800

    feat: add a simple navbar

---
+++ src/lib/components/app/NavBar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import AppIcon from './AppIcon.svelte';
+</script>
+
+<div class="flex items-center w-full">
+	<div class="flex gap-3 items-center justify-center">
+		<AppIcon class="size-12" />
+		<div class="w-52 font-default text-xl text-white flex justify-center items-center">
+			VCE Study Design Browser
+		</div>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import NavBar from '$lib/components/app/NavBar.svelte';
+	import Icon from '../lib/components/app/AppIcon.svelte';
+</script>
+
+<div class="p-4 w-screen h-screen">
+	<NavBar />
+</div>
