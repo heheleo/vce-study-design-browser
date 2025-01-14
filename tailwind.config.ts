1acdbacbf584cc1e624df2a6b04181f8c96034ce
Leo <leowasntfound@gmail.com>
2025-01-14 14:37:23 +0800

    feat: fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,9 @@ const config: Config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+				sans: [...fontFamily.sans],
+				display: ["Inter", ...fontFamily.sans],
+				default: ["Bricolage Grotesque", ...fontFamily.sans]
 			}
 		}
 	},
