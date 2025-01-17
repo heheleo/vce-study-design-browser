bbf90b05b83a2186fb07dd7ce6d092ac77d5b2c9
Leo <leowasntfound@gmail.com>
2025-01-17 16:29:33 +0800

    feat: make topic names stand out

--- src/lib/components/app/selection/AOSContent.svelte
+++ src/lib/components/app/selection/AOSContent.svelte
@@ -6,15 +6,19 @@
 
 <div class="flex flex-col gap-1 font-serif">
 	{#if aosData.points?.length}
-		<div class="text-xl text-primary/90">This area of study covers:</div>
+		<div class="text-xl">This area of study covers:</div>
 		{#each aosData.points as point}
-			<span class="ml-4">• {point}</span>
+			<span class="rounded-sm pl-4 text-primary/85 transition-colors hover:bg-primary/20">
+				• {point}
+			</span>
 		{/each}
 	{:else if aosData.topics?.length}
 		{#each aosData.topics as topic}
-			<div class="text-xl mt-2">{topic.name}</div>
+			<div class="mt-2 text-xl">{topic.name}</div>
 			{#each topic.points as point}
-				<span class="ml-4 text-primary/85">• {point}</span>
+				<span class="rounded-sm pl-4 text-primary/85 transition-colors hover:bg-primary/20">
+					• {point}
+				</span>
 			{/each}
 		{/each}
 	{/if}
